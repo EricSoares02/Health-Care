41fd596ed0ddbd4090303d0ea65df2b17ccbeaa0
Eric Soares <ericsoareslopesdev@gmail.com>
2024-04-17 16:38:44 -0300

    feat: start home page

---
+++ components/HomeSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+defineProps<{
+    title?: string
+}>();
+
+</script>
+
+<template>
+
+    <section class="w-full max-w-screen-2xl h-[524px] mb-10 px-1">
+      <div class="w-full h-12 flex items-center">
+          <h1 class="w-48 h-full flex items-center justify-center font-semibold text-lg text-nowrap pl-4">{{ title }}</h1>
+      </div>
+      <slot></slot>
+    </section>
+
+</template>
--- layouts/custom_layout.vue
+++ layouts/custom_layout.vue
@@ -14,9 +14,9 @@ function ToogleMenu(){
 <div class="w-screnn h-screen">
 
   <DefaultHeader @ToogleMenu="ToogleMenu"/>
-  <main class="w-full h-auto min-h-full relative top-20 flex flex-col px-1  bg-gray-300">
+  <main class="w-full h-auto min-h-full relative top-20 flex flex-col bg-gray-300">
     <aside v-if="menubar"
-    class="w-72 h-full fixed left-0 bg-white-100 pt-2
+    class="w-72 h-full fixed left-0 bg-white-100 pt-2 z-40
     max-md:w-full max-md:h-full 
     ">
       <SideBar/>
